0913c97a0a5f2f796145cff7d3d6a9084b8e9446
kt96951 <kt96951@uga.edu>
2026-04-27 22:27:09 -0400

    updated deseq

--- DEseq_analysis.R
+++ DEseq_analysis.R
@@ -82,10 +82,10 @@ dev.off()
 pCutoff = 0.05
 FCcutoff = 1.0
 rownames(res) <- gsub("gene-", "", rownames(res))
-p = EnhancedVolcano(data.frame(res), lab = rownames(res), x = 'log2FoldChange', y = 'padj',ylim = c(0, 15),
+p = EnhancedVolcano(data.frame(res), lab = rownames(res), x = 'log2FoldChange', y = 'padj',ylim = c(0, 8),
                     xlab = bquote(~Log[2]~ 'fold change'), ylab = bquote(~-Log[10]~adjusted~italic(P)),
                     pCutoff = pCutoff, FCcutoff = FCcutoff, pointSize = 0, labSize = 4.0,col = c('grey30', 'forestgreen', 'royalblue', 'red2'),colAlpha = 1.0 ,shape = 19,
-                    title = "Volcano plot", subtitle = "SSA/P vs. Normal",
+                    title = "Volcano plot", subtitle = "OCA2 mutant vs. Wild Type",
                     caption = paste0('log2 FC cutoff: ', FCcutoff, '; p-value cutoff: ', pCutoff, '\nTotal = ', nrow(res), ' variables'),
                     drawConnectors = TRUE,
                     widthConnectors = 0.5,
